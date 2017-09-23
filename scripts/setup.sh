97e22e20ef9947929febfc3111ffaf853ff4ff7f
Hiroki Sakuraba <sakuraba@tech.gr.jp>
2017-09-24 00:17:55 +0900

    Re create

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,39 +1,50 @@
-#?/bin/sh
+#!/bin/bash
+script_dir=$(cd $(dirname ${BASH_SOURCE:-$0}); pwd)
+cd $script_dir/../
 
-type brew >/dev/null 2>&1
-if [ echo $? -eq 0 ]; then
+read -p "SUDO Password : " password
+read -p "personal or work? : " stage
+
+which brew >/dev/null 2>&1
+if [ $? -ne 0 ]; then
+	echo "Not found brew"
+	echo "Install brew"
 	ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 fi
-while read -p "playbook file? " playbook ; do
-	while read -p "password? " password ; do
-		# pip setup
-		which pip >/dev/null 2>&1
-		if [ $? -ne 0 ]; then
-			echo "not found command pip"
-			echo "install pip"
-			expect -c "
-			set timeout 5
-			spawn sudo easy_install pip
-			expect \"Password:\"
-			send \"${PW}\n\"
-			expect \"$\"
-			exit 0
-			"
-		fi
 
-		# install ansible
-		which ansible >/dev/null 2>&1
-		if [ $? -ne 0 ]; then
-			echo "not found command ansible"
-			echo "install ansible"
-			pip install ansible
-		fi
-		expect -c "
-		set timeout 600
-		spawn ansible-playbook ${playbook} --ask-become-pass
-		expect \"SUDO password:\"
-		send \"${password}\n\"
-		expect \"$\"
-		"
-	done
-done
+which pip >/dev/null 2>&1
+if [ $? -ne 0 ]; then
+	echo "Not found pip"
+	echo "Install pip"
+	expect -c "
+	set timeout 5
+	spawn sudo easy_install pip
+	expect \"Password:\"
+	send \"${password}\n\"
+	expect \"$\"
+	exit 0
+	"
+fi
+
+which ansible >/dev/null 2>&1
+if [ $? -ne 0 ]; then
+	echo "Not found ansible"
+	echo "Install ansible"
+	pip install ansible
+	expect -c "
+	set timeout 5
+	spawn sudo pip install ansible
+	expect \"Password:\"
+	send \"${password}\n\"
+	expect \"$\"
+	exit 0
+	"
+fi
+
+expect -c "
+set timeout 600
+spawn ansible-playbook -i inventories/${stage} --ask-become-pass main.yml
+expect \"SUDO password:\"
+send \"${password}\n\"
+expect \"$\"
+"
