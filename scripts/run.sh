db2e6b159528c3eea275d1bc8e1552bb7e26670c
Hiroki Sakuraba <sakuraba@tech.gr.jp>
2017-11-24 19:41:18 +0900

    Update exec script

--- scripts/run.sh
+++ scripts/run.sh
@@ -10,13 +10,12 @@ function run_sudo() {
 }
 
 function init() {
-	local is_exist_ansible=$(which ansible >/dev/null 2>&1)
-	local is_exist_brew=$(which brew >/dev/null 2>&1)
-	local is_exist_pip=$(which pip >/dev/null 2>&1)
-
-	[ $is_exist_brew -ne 0 ] && ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-	[ $is_exist_pip -ne 0 ] && run_sudo "easy_install pip" $password
-	[ $ansible_status -eq 0 ] && run_sudo "pip install ansible" $password
+	which brew >/dev/null 2>&1
+	[ $? -ne 0 ] && ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+	which pip >/dev/null 2>&1
+	[ $? -ne 0 ] && run_sudo "easy_install pip" $password
+	which ansible >/dev/null 2>&1
+	[ $? -eq 0 ] && run_sudo "pip install ansible" $password
 }
 
 echo -n 'SUDO Password :'
