9123dcbaec009e5c1b2aa44b2ba3387fd37f562f
Hiroki Sakuraba <sakuraba@tech.gr.jp>
2017-12-10 01:15:30 +0900

    Add terraform provider

--- cloud.tf
+++ cloud.tf
@@ -8,6 +8,25 @@ terraform {
 
 variable "digitalocean_token" {}
 
+variable "aws_access_key_id" {}
+variable "aws_secret_access_key" {}
+variable "aws_region" {}
+variable "google_credentials" {}
+variable "google_project" {}
+variable "google_region" {}
+
 provider "digitalocean" {
   token = "${var.digitalocean_token}"
+}
+
+provider "aws" {
+  access_key = "${var.aws_access_key_id}"
+  secret_key = "${var.aws_secret_access_key}"
+  region     = "${var.aws_region}"
+}
+
+provider "google" {
+  credentials = "${var.google_credentials}"
+  project     = "${var.google_project}"
+  region      = "${var.google_region}"
 }
